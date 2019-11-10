558acf2eff6de021facf99591a0e1e01cdb7ab00
jurek55 <jerzy.kunicki@gmail.com>
2019-11-10 23:57:37 +0100

    Update paneladmin.php

--- paneladmin.php
+++ paneladmin.php
@@ -23,10 +23,23 @@ function Logowanie(){
     if (!$polaczenie->connect_errno){
         $user=$_POST['user'];
         $password=$_POST['password'];
-        $hash=password_hash($password, PASSWORD_DEFAULT);
-        $email=$_POST['email'];
-
-    $sql="SELECT * FROM uzytkownicy WHERE user=$user";
+        //$email=$_POST['email'];
+    $sql="SELECT * FROM uzytkownicy WHERE user='$user'";
     $wynik=$polaczenie->query($sql);
     $ile_wierszy=$wynik->num_rows;
+    if ($ile_wierszy>0){
+        $wiersz=$wynik->fetch_assoc();
+        if (password_verify($password, $wiersz['haslo'])){
+            echo 'Witaj '.$user.', masz dostęp do bazy';
+        } else {
+            echo 'Nie masz uprawnień do pracy z bazą';
+        }
+    }
+    
+   
+
 }
+}
+//rejestracja();
+
+Logowanie();
