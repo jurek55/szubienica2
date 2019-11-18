64262143bbc872e52ea7f1ca1fa056e870a325e1
jurek55 <jerzy.kunicki@gmail.com>
2019-11-18 22:00:34 +0100

    Update kategorie_poczatek.js

--- kategorie_poczatek.js
+++ kategorie_poczatek.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     var tekst='';
     var i=1;
     while (obj_kateg[i]){
-        tekst=tekst+'<li><a href="start.php?kategoria='+i+'">'+obj_kateg[i]+'</li>';
+        tekst=tekst+'<li><a href="start.php?kategoria='+i+'">'+obj_kateg[i]+'</a></li>';
         i++;
     }
     $("ol").html(tekst);
