58252724505e7964d95b7ebe2c0af518fde57c4e
jurek55 <jerzy.kunicki@gmail.com>
2019-11-18 22:01:10 +0100

    Update poczatek.php

--- poczatek.php
+++ poczatek.php
@@ -8,30 +8,33 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="pl">
     
-<head>
-    <meta charset="utf8">
-    <title>Szubienica</title>
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700&display=swap&subset=latin-ext" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <script rel="javascript" src="jquery321.js"></script>
-    <script rel="javascript" src="klawiaturanieakt.js" type="text/javascript"></script>
-    <script rel="javascript" src="kategorie_poczatek.js" type="text/javascript"></script>
-</head>
+    <head>
+        <meta charset="utf8">
+        <title>Szubienica</title>
+        <meta name="viewport" content="width=device-width"/>
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700&display=swap&subset=latin-ext" rel="stylesheet"/>
+        
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <script rel="javascript" src="jquery321.js"></script>
+        <script rel="javascript" src="klawiaturanieakt.js" type="text/javascript"></script>
+        <script rel="javascript" src="kategorie_poczatek.js" type="text/javascript"></script>
+    </head>
 
-<body>
-<input type="hidden" id="spis_kategorii" value='<?php echo $_SESSION['json_kat'] ?>'>
-<div class="container">
-<header>HASŁO DO ODGADNIĘCIA</header>
-<div class="haslo">--kie- t-m ha-ło</div>
-<div class="kategoria">
-    <header>Wybierz kategorię:</header>
-<ol></ol>
-</div>
-<div class="obraz"><img src="img/ojej.png"></div>
-<div class="klawiatura"></div>
-</div>
-
-</body>
+    <body>
+        <input type="hidden" id="spis_kategorii" value='<?php echo $_SESSION['json_kat'] ?>'/>
+        
+        <div class="container">
+        <header>HASŁO DO ODGADNIĘCIA</header>
+        <div class="haslo">--kie- t-m ha-ło</div>
+                <div class="kategoria">
+                    <header>Wybierz kategorię:</header>
+                    <ol></ol>
+                </div>
+            <div class="image"><img src="img/ojej.png"></div>
+            <div class="okladka klawisze"></div>
+            <div class="break"></div>
+        </div>
+    </body>
 </html>
